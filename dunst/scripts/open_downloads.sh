991e6e0c2bd1823926ff06b0a0ba2bc3af95ccaa
crisxdwars <crisxdwars@gmail.com>
2026-05-14 16:09:56 +0800

    Fixed format

--- dunst/scripts/open_downloads.sh
+++ dunst/scripts/open_downloads.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 FILE=$(echo "$DUNST_BODY" | tail -n 1)
-
 if [[ "$DUNST_BODY" =~ \.(pdf|png|jpe?g)$ ]]; then
     ACTION=$(notify-send --replace-id="$DUNST_ID" -a Vivaldi "$FILE" --action="vivaldi" --icon=/usr/share/pixmaps/vivaldi.png)
-
     case "$ACTION" in
     "0")
         vivaldi "$HOME/Downloads/$FILE"
