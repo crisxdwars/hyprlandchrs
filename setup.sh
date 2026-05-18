885c70adedfb5ac10afc99e4a40de5ef338f731b
crisxdwars <crisxdwars@gmail.com>
2026-05-18 08:04:11 +0800

    AUTOMATIC SETUP: Added Auto-Install Dependencies.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/env bash
 
+echo "Installing Dependencies..."
+
+sudo pacman -S waybar swaybg gpu-screen-recorder wl-clipboard slurp grim ttf-fira-code ttf-fira-code noto-fonts-cjk zip unzip pavucontrol --noconfirm
+
 mkdir -p ~/.config
 cp -r wofi ~/.config
 cp -r dunst ~/.config
